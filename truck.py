e9c212eb82d3c821c5fd6c67c33841220a7e982d
baileyb <38264710+bailey-bo@users.noreply.github.com>
2023-04-24 09:14:47 -0700

    Delete truck.py

--- truck.py
+++
@@ -1,7 +0,0 @@
-class Truck:
-    # Constructor used to store packages to truck objects
-    def __init__(self, packages, distance, timeDone):
-        self.packages = packages
-        self.distance = distance
-        self.timeDone = timeDone
-
